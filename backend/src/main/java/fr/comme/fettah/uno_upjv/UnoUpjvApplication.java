ffc2c1d5baa821c9139de8de7f30e8f5691a2ecf
matthieu-comme <comme.matthieu@gmail.com>
2026-02-11 13:59:33 +0100

    structure repo

---
+++ backend/src/main/java/fr/comme/fettah/uno_upjv/UnoUpjvApplication.java
@@ -0,0 +1,13 @@
+package fr.comme.fettah.uno_upjv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnoUpjvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnoUpjvApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/fr/comme/fettah/uno_upjv/UnoUpjvApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.comme.fettah.uno_upjv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnoUpjvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
