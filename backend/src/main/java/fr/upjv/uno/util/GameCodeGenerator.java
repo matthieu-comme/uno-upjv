c66f8f32ffdba5771debe528584442b433027c69
Matthieu C. <comme.matthieu@gmail.com>
2026-03-27 23:34:12 +0100

    longueur gameCode diminué à 4 char

--- backend/src/main/java/fr/upjv/uno/util/GameCodeGenerator.java
+++ backend/src/main/java/fr/upjv/uno/util/GameCodeGenerator.java
@@ -7,7 +7,7 @@ import java.security.SecureRandom;
  */
 public class GameCodeGenerator {
   private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  private static final int CODE_LENGTH = 8;
+  private static final int CODE_LENGTH = 4;
   private static final SecureRandom RANDOM = new SecureRandom();
 
   /**
